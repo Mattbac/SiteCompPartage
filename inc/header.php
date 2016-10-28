7d754e2385bdc76d6874b9233ff7aa1f85b852c7
Etudiant <Etudiant>
2016-10-28 14:18:16 +0200

    MaJ int

---
+++ inc/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>SiteCompPartage</title>
+
+    <!-- ### LINKS ### -->
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Signika" rel="stylesheet">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/main.css">
+
+    <!-- ### LINKS : END ### -->
+
+</head>
+<body>
+
+<header>
+
+    <!-- ### BANNER + LOGO ### -->
+    
+    <div id="logo">
+        <img src="../img/logo.png" alt="SUPERLOGO">
+    </div>
+
+    <div id="banner">
+        <img src="../img/banner.jpg" alt="Bannière">
+    </div>
+
+    <!-- ### NAVBAR ### -->
+
+    <nav id="navbar">
+        <ul>
+            <li>
+              <a href="#">Le jeu</a>
+            </li>
+            <li>
+              <a href="#">Médias</a>
+            </li>
+            <li>
+              <a href="#">Le staff</a>
+            </li>
+            <li>
+              <a href="#">Forum</a>
+            </li>
+            <li>
+              <a href="#">Assistance</a>
+            </li>
+        </ul>
+    </nav>
+
+</header>
