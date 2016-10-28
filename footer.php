b88127f8a1c3a391e1a26d7c70434f366e23713c
Etudiant <Etudiant>
2016-10-28 10:33:52 +0200

    header + footer = <3

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!-- ### SCRIPT LAND ### -->
+
+<script src="jquery-3.1.1.min.js"></script>
+<script src="main.js"></script>
+
+<!-- ### SCRIPT LAND : END -->
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>SiteCompPartage</title>
+    
+    <!-- ### LINKS ### -->
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Signika" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+    
+    <!-- ### LINKS : END ### -->
+    
+</head>
+<body>
+
+<header>
+    
+    <div id="banner">
+        <img src="#" alt="Bannière">
+    </div>
+    
+    <nav>
+        <ul>
+            <li><a href="#">Le jeu</a></li>
+            <li><a href="#">Médias</a></li>
+            <li><a href="#">Le staff</a></li>
+            <li><a href="#">Forum</a></li>
+            <li><a href="#">Assistance</a></li>
+        </ul>
+    </nav>
+    
+</header>
